29bb978bc69cefd82be9a534060ce425ef2ec876
KhunKyawTunwin <kyawtunwin000782@gmail.com>
2024-06-27 12:17:11 +0700

    format_string

---
+++ format_string.py
@@ -0,0 +1,8 @@
+age = 30
+
+str_data = "Hello Khun"
+
+print(str_data + "My age is : " + str(age) + " years old." )
+
+# format string
+print(f"My age is {age} years old. My birthday years is {2024 - 29}")
