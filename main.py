d76a45e1e4825eb0e842dd0a186f02cbca9fc8d2
KhunKyawTunwin <kyawtunwin000782@gmail.com>
2024-06-27 12:02:57 +0700

    New Programming with PY

---
+++ main.py
@@ -0,0 +1,12 @@
+# print ("Hello new programming with python") # print hello world to console
+
+# name = input("Name : ")
+# print("Hello " + name)
+
+
+PI = 3.14
+# int ( type casting )
+r = int (input("Radis : ")) # input value always return only string value ( without type casting )
+ 
+area = PI * r**2;
+print( area)
